6a88ecbf68675f8220260ba27b2e204e207f32da
Flonator3000 <florian.woerle@yahoo.de>
2022-11-16 15:52:21 +0100

    postman key

--- src/main/java/secret.java
+++ src/main/java/secret.java
@@ -1,5 +1,5 @@
 class Secret {
     String postmanKey = "PMAK-6374ef43bbe6b674b399826e-cb8d5b87d13c4b1d6a1855dca73af0e231";
 
-
+    String postmanKey2 = "PMAK-6374ef43bbe6b674b399826e-cb8d5b87d13c4b1d6a1855dca73af0e231";
 }
